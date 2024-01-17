9e506c36dbdd9807a8e945b7748c5ebcb8e497f7
NZC\Aamir.Mulla <aamir.mulla@2degrees.nz>
2024-01-17 16:52:41 +1300

    Fix and add tests

--- Awesome.UnitTests/HelloWorld/HelloWorldTests.cs
+++ Awesome.UnitTests/HelloWorld/HelloWorldTests.cs
@@ -1,6 +1,7 @@
 ﻿using Awesome.HelloWorld;
 using Microsoft.Extensions.Options;
 using Moq;
+
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace Awesome.UnitTests.HelloWorld;
@@ -8,24 +9,42 @@ namespace Awesome.UnitTests.HelloWorld;
 public class HelloWorldTests
 {
     private IHelloWorldMessageService _helloWorldMessageService;
+    private readonly Mock<IOptions<HelloWorldOptions>> _helloWorldOptions = new();
 
     [SetUp]
     public void Setup()
     {
-        var helloWorldOptions = new Mock<IOptions<HelloWorldOptions>>();
-        _helloWorldMessageService = new HelloWorldMessageService(helloWorldOptions.Object);
     }
 
     [Test]
-    public void HelloWorld_SaysStandardHelloWorld_WhenSayHelloToTheWorld_IsCalled()
+    public void HelloWorld_SaysStandardHelloWorld_WhenSayHelloToTheWorld_IsCalled_WithoutSettings()
     {
         // Arrange
         var expectedMessage = "Hello World!";
-        
+        _helloWorldOptions.SetupGet(o => o.Value)
+            .Returns(new HelloWorldOptions());
+        _helloWorldMessageService = new HelloWorldMessageService(_helloWorldOptions.Object);
+
         // Act
         var helloMessageToTheWorld = _helloWorldMessageService.GetHelloMessageToTheWorld();
 
         //Assert
-        Assert.Equals(helloMessageToTheWorld, expectedMessage);
+        Assert.That(expectedMessage, Is.EqualTo(helloMessageToTheWorld));
+    }
+
+    [Test]
+    public void HelloWorld_SaysStandardHelloWorld_WhenSayHelloToTheWorld_IsCalled_WithSettings()
+    {
+        // Arrange
+        var expectedMessage = "Hello Earth!";
+        _helloWorldOptions.SetupGet(o => o.Value)
+            .Returns(new HelloWorldOptions { Message = "Hello Earth!" });
+        _helloWorldMessageService = new HelloWorldMessageService(_helloWorldOptions.Object);
+
+        // Act
+        var helloMessageToTheWorld = _helloWorldMessageService.GetHelloMessageToTheWorld();
+
+        //Assert
+        Assert.That(helloMessageToTheWorld, Is.EqualTo(expectedMessage));
     }
 }
